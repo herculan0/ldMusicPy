c61c528acbdee105f07910c783afeccf3b0adb96
herculan0 <lucasgherculano@gmail.com>
2020-04-21 16:07:16 -0300

    Alterações para testar cadastro no banco

--- app/ldm.py
+++ app/ldm.py
@@ -1,25 +1,29 @@
-# coding:utf-8
 from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
-import pymysql.cursors as cursors
+import pymysql
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:ldmusic@db:3306/ldMusic'
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:ldmusic@db:3306/ldMusic?charset=utf8mb4'
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.secret_key = 'ldmusic2020'
 
 db = SQLAlchemy(app)
 
-class Administrador(db.Model):
-    __tablename__ = 'administrador'
+class Aluno(db.Model):
+    __tablename__ = 'aluno'
     __id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    nome = db.Column(db.String(16))
-    email = db.Column(db.String(100))
+    nome = db.Column(db.String(64))
+    email = db.Column(db.String(45))
+    senha = db.Column(db.String(11))
 
-    def __init__(self, nome, email):
+    def __init__(self, nome, email, senha):
         self.nome = nome
         self.email = email
+        self.senha = senha
 
 
 db.create_all()
@@ -36,16 +40,22 @@ def cadastrar():
 @app.route("/cadastro", methods=['GET','POST'])
 def cadastro():
     if request.method == "POST":
+
         nome = request.form.get("nome")
         email = request.form.get("email")
+        senha = request.form.get("senha")
 
-        if nome and email:
-            admin = Administrador(nome, email)
-            db.session.add()
+        if nome and email and senha :
+            aluno = Aluno(nome,email, senha)
+            db.session.add(aluno)
+            db.session.commit()
 
-@app.route("/aluno")
-def aluno():
-    return render_template("aluno.html")
+    return redirect(url_for("index"))
+
+@app.route("/lista")
+def lista():
+    alunos = Aluno.query.all()
+    return render_template("lista.html",alunos=alunos)
 
 if __name__=='__main__':
     app.run(debug=True, host='0.0.0.0')
