66c21cc5b314d5ed6ebf95b162a4e64c3e575676
herculan0 <lucasgherculano@gmail.com>
2020-04-26 02:03:30 -0300

    criado routes.py para modularizar a aplicação, começado a criar a rota para instrumentos

--- app/ldm.py
+++ app/ldm.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 import pymysql
+import routes
 
 app = Flask(__name__)
 
@@ -23,7 +24,16 @@ app.secret_key = 'ldmusic2020'
 
 db = SQLAlchemy(app)
 
+class Instrumento(db.Model):
+
+
 #CRIANDO CLASSE ALUNO#
+    __tablename__='instrumento'
+    cod_instrumento = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nome_instrumento = db.Column(db.String(45))
+
+    def __init__(self, nome_instrumento):
+        self.nome_instrumento = nome_instrumento
 
 class Aluno(db.Model):
 
@@ -41,64 +51,6 @@ class Aluno(db.Model):
 
 #CRIA O BANCO DE DADOS DE ACORDO COM O INSTRUÍDO NOS BLOCOS ACIMA:(PODERÁ SER DELETADO, CASO O BANCO DE DADOS JÁ ESTEJA PRONTO)#
 db.create_all()
-
-@app.route("/")
-def index():
-    return render_template("main.html")
-
-@app.route("/cadastrar")
-def cadastrar():
-    return render_template("cadastro.html")
-
-#ROTA PARA CREATE-RUD
-@app.route("/cadastro", methods=['GET','POST'])
-def cadastro():
-    if request.method == "POST":
-
-        nome = request.form.get("nome")
-        email = request.form.get("email")
-        senha = request.form.get("senha")
-
-        if nome and email and senha :
-            aluno = Aluno(nome,email, senha)
-            db.session.add(aluno)
-            db.session.commit()
-
-    return redirect(url_for("index"))
-#ROTA PARA C-READ-UD#
-@app.route("/lista")
-def lista():
-    alunos = Aluno.query.all()
-    return render_template("lista.html",alunos=alunos)
-
-#ROTA PARA CRU-DELETE#
-@app.route("/excluir/<int:id>")
-def excluir(id):
-    aluno = Aluno.query.filter_by(_id=id).first()
-    db.session.delete(aluno)
-    db.session.commit()
-
-    alunos = Aluno.query.all()
-    return render_template("lista.html", alunos=alunos)
-
-@app.route("/atualizar/<int:id>", methods=['GET', 'POST'])
-def atualizar(id):
-    aluno = Aluno.query.filter_by(_id=id).first()
-
-    if request.method == "POST":
-        nome = request.form.get("nome")
-        email = request.form.get("email")
-        senha = request.form.get("senha")
-
-        if nome and email and senha :
-            aluno.nome = nome
-            aluno.email = email
-
-            db.session.commit()
-
-            return redirect(url_for("lista"))
-
-    return render_template("atualizar.html", aluno=aluno)
 
 if __name__=='__main__':
     app.run(debug=True, host='0.0.0.0')
