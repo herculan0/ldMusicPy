8b2684ee94656530ef62ea060879173241d5c493
herculan0 <lucasgherculano@gmail.com>
2020-05-02 19:55:54 -0300

    arquivo de configuração

--- app/trashy/bkp__init__.py
+++
@@ -1,9 +0,0 @@
-
-
-
-def create_app(config_name):
-
-    from .auth import auth_blueprint
-    app.register_blueprint(auth_blueprint, url_prefix='/auth')
-
-    return app
--- app/trashy/routes/index.py
+++
@@ -1,6 +0,0 @@
-from flask import render_template
-from . import rotas
-
-@rotas.route("/")
-def index():
-     return render_template("main.html")
