3fbffa7cf74eb2226f4c7dc44968313670930cfb
herculan0 <lucasgherculano@gmail.com>
2020-05-03 19:21:32 -0300

    criado arquivo de configuração da aplicação

--- config.py
+++ config.py
@@ -3,17 +3,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or ('ldmusic')
-    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', true).lower() in ['true', 'on', 1]
+    MAIL_SERVER = os.environ.get('MAIL_SERVER') or ('smtp.googlemail.com')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or ('587'))
+#    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', true).lower() in ['true', 'on', 1]
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     LDM_EMAIL_SUBJECT_PREFIX =  '[ldMusic]'
     LDM_MAIL_SENDER = 'Administrador ldMusic <ldmusic00@gmail.com>'
     LDM_ADMIN = os.environ.get('LDM_ADMIN')
-    SQLALCHEMY_TRACK_MODIFICATIONS = FALSE
-
-    @staticmethod
-    def init_app(app)
+#    SQLALCHEMY_TRACK_MODIFICATIONS = FALSE
 
 class  DevelopmentConfig(Config):
     DEBUG = True
