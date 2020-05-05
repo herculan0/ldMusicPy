9174717e0ddf05ad3ed1f5c47046b91fed45be7a
herculan0 <lucasgherculano@gmail.com>
2020-05-04 22:32:31 -0300

    rota de template de autenticacao está funcionando, necessário consertar o formulário de cadastro e criar template de login, muitas falhas há serem consertadas

--- ldMusicPy/autenticacao.py
+++ ldMusicPy/autenticacao.py
@@ -1,9 +1,45 @@
 import functools
-from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
-)
-from werkzeug.security import check_password_hash, generate_password_hash
-from .models import CadastroForm, LoginForm
+from flask import Blueprint
+from flask import flash
+from flask import g
+from flask import redirect
+from flask import render_template
+from flask import request
+from flask import session
+from flask import url_for
+from werkzeug.security import check_password_hash
+from werkzeug.security import generate_password_hash
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms import PasswordField
+from wtforms import BooleanField
+from wtforms import SubmitField
+from wtforms import RadioField
+from wtforms import PasswordField
+from wtforms.validators import DataRequired
+from wtforms.validators import Length
+from wtforms.validators import Email
+from wtforms.validators import URL
+from wtforms.validators import EqualTo
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Length(1,64), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired()])
+    remember_me = BooleanField('Me mantenha logado!')
+    submit = SubmitField('Entrar')
+
+
+class CadastroForm(FlaskForm):
+    primeiroNome = StringField('Primeiro Nome:', validators=[DataRequired()])
+    ultimoNome = StringField('Último Nome:')
+    email = StringField('Email:', validators=[Email(),
+                                              DataRequired()])
+    senha = PasswordField('Senha: ', validators=[DataRequired(),
+                                                 EqualTo('confirmaSenha', message='As senhas devem ser Iguais')])
+    confirmaSenha = PasswordField('Confirmar Senha ')
+    funcao = RadioField('Aluno ou Instrutor', validators=[DataRequired()])
+    videoURL = StringField('Link do Vídeo: ', validators=[URL()])
+    submit = SubmitField('Cadastrar')
 
 bp = Blueprint('autenticacao', __name__, url_prefix='/autenticacao')
 
@@ -14,7 +50,7 @@ def cadastro():
     if 'email' in session:
         return redirect(url_for('perfil'))
 
-    if requested.method == 'POST':
+    if request.method == 'POST':
         if form.validade == False:
             return render_template('cadastro.html', form=form)
         else:
@@ -31,7 +67,7 @@ def cadastro():
             session['email'] = usuario.email
             return redirect(url_for('perfil'))
 
-    return render_template('cadastro.html', form=form)
+    return render_template('autenticacao/cadastro.html', form=form)
 
 @bp.before_app_request
 def carregar_usuario_logado():
