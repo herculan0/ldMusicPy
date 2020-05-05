d959dd52d2bfb3f4832698a7c119168bc44d5686
herculan0 <lucasgherculano@gmail.com>
2020-05-04 22:35:46 -0300

    arquivos de configuração

--- setup.py
+++ setup.py
@@ -1,10 +1,3 @@
 from setuptools import setup
 
-setup(
-    name='ldMusicPy',
-    packages=['ldMusicPy'],
-    include_package_data=True,
-    install_requires=[
-        'flask',
-    ],
-)
+setup()
