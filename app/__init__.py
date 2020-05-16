1de8ee41841cee52fca7eefb042704c17fbc7481
Herculano <lucasgherculano@gmail.com>
2020-05-16 17:33:46 -0300

    testando novas features

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,15 @@
-from flask import Flask
+import os
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
     return "Olá, usuário!"
+
+@app.route('/<usuario>')
+def perfil():
+    return jsonify(nome="{}".format(usuario))
