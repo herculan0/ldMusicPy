55527e49194141097e85ff25c3756b7b902a4a05
Herculano <lucasgherculano@gmail.com>
2020-05-17 09:59:10 -0300

    Adicionado Permissoes e Funcoes, Usuario e Administrador

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 import os
-import hashlib ## senha_hash
-
+import hashlib
+import bleach
 from dotenv import load_dotenv
 dotenv_path = os.path.join(os.path.dirname(__file__) '.env.')
 
@@ -11,6 +11,9 @@ from flask_login import LoginManager, UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from datetime import datetime
+
+
 
 ### instancia um objeto da aplicação chamado app ##
 app = Flask(__name__)
@@ -34,6 +37,61 @@ def index():
 
 ## MODELS ###
 
+### cria classe para dar permissões aos usuários ###
+class Permissao:
+    COMENTAR = 2
+    ESCREVER = 4
+    ADMIN = 16 
+
+
+### classe funcao, onde invoca permissoes para admin ou usuario comum ###
+class Funcao(db.Model):
+    __tablename__ = 'funcao'
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(64), unique=True)
+    padrao = db.Column(db.Boolean, default=False, index=True)
+    permissoes = db.Column(db.Integer)
+    usuario = db.relationship('Usuario', backref='funcao', lazy='dynamic')
+
+    def __init__(self, **kwargs):
+        super(Funcao, self).__init__(**kwargs)
+        if self.permissoes is None:
+            self.permissions = 0
+
+    @staticmethod
+    def inserir_funcoes():
+        funcoes = {
+            'Usuario': [Permissao.COMENTAR,
+                        Permissao.ESCREVER],
+            'Administrador':[Permissao.COMENTAR,
+                             Permissao.ESCREVER,
+                             Permissao.ADMIN]
+        }
+        funcao_padrao = 'Usuario'
+        for f in funcoes:
+            funcao = Funcao.query.filter_by(nome=f).first()
+            if funcao is None:
+                funcao = Funcao(nome=f)
+            funcao.resetar_permissoes(perm)
+            for perm in funcoes[f]:
+                funcao.add_permissao(perm)
+            funcao.padrao = (funcao.nome == funcao_padrao)
+            db.session.add(funcao)
+        db.session.commit()
+
+    def add_permissao(self, perm):
+        if not self.tem_permissao(perm):
+            self.permissoes += perm
+
+    def remover_permissao(self):
+        self.permissoes = 0
+
+    def tem_permissao(self, perm):
+        return self.permissoes & perm == perm
+
+    def __repr__(self):
+        return '<Funcao %f>' % self.nome
+
 ### classe usuário ###
 class Usuario(UserMixin,db.Model):
     __tablename__ = 'usuario'
@@ -47,22 +105,26 @@ class Usuario(UserMixin,db.Model):
     confirmado = db.Column(db.Boolean, default=False)
     def __init__(self, **kwargs):
         super(Usuario, self).__init__(**kwargs)
-
+    
     @propriedade
     def senha(self):
         raise AttributeError('Senha não é um atributo legível.')
 
+    ### gera o hash da senha para guardar no banco ###
     @senha.setter
     def senha(self, senha):
         self.senha_hash = generate_password_hash(senha)
     
+    ### verifica se o hash bate com a senha ###
     def verifica_senha(self, senha):
         return check_password_hash(self.senha_hash, senha)
 
+    ### gerar token para confirmação de email ###
     def gerar_token_confirmar(self, token):
         s = Serializer(current_app.config['SECRET_KEY'], expiration)
         return s.dumps({'confirma': self.id}).decode('utf-8')
-
+    
+    ### usuário precisa confirmar no email o cadastro ###
     def confirmar(self, token):
         s = Serializer(current_app.config['SECRET_KEY'])
         try:
@@ -75,10 +137,12 @@ class Usuario(UserMixin,db.Model):
         db.session.add(self)
         return True
 
+    ### gerar o token para reset de senha ###
     def gerar_reset_token(self, expiration=3600):
         s = Serializer(current_app.config['SECRET_KEY'], expiration)
         return s.dumps({'reset': self.id}).decode('utf-8')
 
+    ### recriar senha usuário ###
     @staticmethod
     def reset_senha(token, nova_senha):
         s = Serializer(current_app.config['SECRET_KEY'])
@@ -93,12 +157,14 @@ class Usuario(UserMixin,db.Model):
         db.session.add(usuario)
         return True
 
-
+### cria usuário anônimo retornando falso para qualquer permissão ###
 class UsuarioAnonimo(AnonymousUserMixin):
     def can(self,permissoes):
         return False
     def administrador:
         return False
+
+login_manager.anonymous_user = UsuarioAnonimo
 
 ## método para carregar usuário ##
 @login_manager.user_loader
