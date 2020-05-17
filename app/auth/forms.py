ceb7613e3859145b780d0865762f29deb0c6d740
Herculano <lucasgherculano@gmail.com>
2020-05-17 10:22:58 -0300

    Estou colocando tudo no __init__ para depois ser modularizado em Blueprints e arquivos como models.py, etc

--- app/auth/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired, Email, Password
-
-class CadastroForm(FlaskForm):
-    username = StringField('Usuario', validators=[DataRequired()])
-    nome = StringField('Primeiro Nome', validators=[DataRequired()])
-    email = StringField ('Email', validators=[DataRequired(), Email])
-    senha = StringField ('Senha', validators=[DataRequired(), Password])
