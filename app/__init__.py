29e7bee42db8a125df4c17c7342002194b44bb5a
Herculano <lucasgherculano@gmail.com>
2020-05-16 21:14:53 -0300

    Funções para autenticação de senha e geração de token para reset e para confirmação

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 import os
+import hashlib ## senha_hash
 
-from flask import Flask
+from flask import Flask, request, url_for, current_app
 from flask_sqlalchemy import SQLAlchemy ## importar banco
 from flask_login import LoginManager, UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
 ### instancia um objeto da aplicação chamado app ##
 app = Flask(__name__)
@@ -40,3 +43,47 @@ class Usuario(UserMixin,db.Model):
     def __init__(self, **kwargs):
         super(Usuario, self).__init__(**kwargs)
 
+    @propriedade
+    def senha(self):
+        raise AttributeError('Senha não é um atributo legível.')
+
+    @senha.setter
+    def senha(self, senha):
+        self.senha_hash = generate_password_hash(senha)
+    
+    def verifica_senha(self, senha):
+        return check_password_hash(self.senha_hash, senha)
+
+    def gerar_token_confirmar(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'confirma': self.id}).decode('utf-8')
+
+    def confirmar(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token.encode('utf-8'))
+        except:
+            return False
+        if data.get('confirmado') != self.id:
+            return False
+        self.confirmado = True
+        db.session.add(self)
+        return True
+
+    def gerar_reset_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'reset': self.id}).decode('utf-8')
+
+    @staticmethod
+    def reset_senha(token, nova_senha):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token.encode('utf-8'))
+        except:
+            return False
+        usuario = Usuario.query.get(data.get('reset'))
+        if user is None:
+            return False
+        usuario.senha = nova_senha
+        db.session.add(usuario)
+        return True
