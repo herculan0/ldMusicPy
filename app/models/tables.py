286d9a011dd8dd919fc49ecfabf0f14e547eb4f4
diovanedm <diovane.dm@gmail.com>
2020-05-30 10:12:52 -0300

    Tabelas construidas, relacionamentos e funcoes a serem criadas

--- app/models/tables.py
+++ app/models/tables.py
@@ -1,9 +1,14 @@
+from app import db
+
 class Administrador(db.Model):
     __tablename__ = "administrador"
 
     id = db.Column(db.Integer, primary_key=True)
     nome_adm = db.Column(db.String(50), nullable=False)
     email_adm = db.Column(db.String(120), nullable=False, unique=True)
+    
+    login_id = db.Column(db.Integer, db.ForeignKey('login.id'))
+    login = db.relationship('Login', backref='administrador', lazy=True)
 
 class Login(db.Model):
     __tablename__ = "login"
@@ -13,7 +18,7 @@ class Login(db.Model):
     senha_login = db.Column(db.String(120))
 
 class Instrutor(db.Model):
-    __tablename__ = "login"
+    __tablename__ = "instrutor"
 
     id = db.Column(db.Integer, primary_key=True)
     firstName = db.Column(db.String(45), nullable=False)
@@ -21,3 +26,29 @@ class Instrutor(db.Model):
     email_instrutor = db.Column(db.String(120), nullable=False, unique=True)
     tel_instrutor = db.Column(db.Integer, nullable=False, unique=True)
 
+class Aluno(db.Model):
+    __tablename__ = "aluno"
+
+    id = db.Column(db.Integer, primary_key=True)
+    firstName = db.Column(db.String(45), nullable=False)
+    lastName = db.Column(db.String(45), nullable=False)
+    email_aluno = db.Column(db.String(120), nullable=False, unique=True)
+    tel_aluno = db.Column(db.Integer, nullable=False, unique=True)
+
+class Instrumento(db.Model):
+    __tablename = "instrumento"
+    id = db.Column(db.Integer, primary_key=True)
+    nome_instrumento = db.Column(db.string(20))
+
+class Aluno_instrumento(db.Model):
+    __tablename__ = "aluno_instrumento"
+
+class Instrutor_instrumento(db.Model):
+    __tablename__ = "instrutor_instrumento"
+
+class Localizacao(db.Model):
+    __tablename__ "localizacao"
+
+    id = db.Column(db.Integer, primary_key=True)
+    latitude = db.Column(db.Float, nullable=False)
+    longitude = db.Column(db.Float, nullable=False)
