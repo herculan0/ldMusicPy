3c5d340fa37612b30218375f6bc9381791e8c8e8
diovanedm <diovane.dm@gmail.com>
2020-06-05 17:13:49 -0300

    Usando o Flask-WTF

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField, SelectField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, Regexp, Email
 
 
 class EditarPerfilForm(FlaskForm):
@@ -10,5 +10,28 @@ class EditarPerfilForm(FlaskForm):
     submit = SubmitField('Atualizar')
 
 class Relatorio(FlaskForm):
-    filtro = SelectField('Filtro', choices=[('n', 'Nome'),('instrumento', 'Instrumento'),('cidade', 'Cidade')])
+    filtro = SelectField('Filtro', choices=[('nome', 'Nome'),('instrumento', 'Instrumento'),('cidade', 'Cidade')])
     busca = StringField('Busca', validators=[Length(1, 180)])
+
+class PerfilAdministrador(FlaskForm):
+    name = StringField('Nome', validators=[DataRequired()])
+    sobrenome = StringField('Sobrenome', validators=[DataRequired()])
+    endereco = StringField('Endereço', validators=[Length(1, 180)])
+    username = StringField(
+        "Usuario",
+        validators=[
+            DataRequired(),
+            Length(1, 64),
+            Regexp(
+                "^[A-Za-z][A-Za-z0-9.]*$",
+                0,
+                "Nomes de Usuário devem conter somente letras,números ou pontos",
+            ),
+        ],
+    )
+    email = StringField(
+        "Email", validators=[DataRequired(), Length(1, 64), Email()]
+    )
+    endereco = TextAreaField(
+        "Endereço", validators=[DataRequired(), Length(1, 180)]
+    )
