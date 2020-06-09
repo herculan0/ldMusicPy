c923623a5d36c4118b97812334751a5c38c1d066
diovanedm <diovane.dm@gmail.com>
2020-06-09 19:45:44 -0300

    Mechendo nos formularios

--- app/main/views.py
+++ app/main/views.py
@@ -18,8 +18,17 @@ def index():
 
 @main.route("/perfil_usuario/", methods=['GET', 'POST'])
 def perfil_usuario():
+    form = EditarPerfilForm()
     usuario = PerfilUsuario()
-    return render_template("perfil_usuario.html", usuario = usuario)
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.endereco = form.endereco.data
+        current_user.sobre_mim = form.sobre_mim.data
+        db.session.add(current_user._get_current_object())
+        db.serssion.commit()
+        flash("Seu perfil foi atualizado com sucesso")
+        return redirect(url_for('.user', username=current_user.username))
+    return render_template("perfil_usuario.html", usuario=usuario, form=form)
 
 @main.route("/perfil_administrador/", methods=['GET', 'POST'])
 def perfil_administrador():
